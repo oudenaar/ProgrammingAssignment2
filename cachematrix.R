b19196d97c093f21fa09f079987113c9e89c4d41
bvoudenaarde <oudenaar@gmail.com>
2015-08-23 22:49:44 +0200

    Add my tested inverse calculation with a special cached matrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creating of cachable matrix with Inverse placeholder.
+# calculating inverse matrix is expensive, this special function create a cachable object
 
-## Write a short comment describing this function
+## Here we give an example of calculating the inverse of a matrix and show the usages of this special functions:
+#
+# B = matrix( c(4,2,7,6),nrow=2, ncol=2)
+# C <- makeCacheMatrix(B)
+# Binv <- cacheSolve(C)
 
+## Calculating true matrix multiplication:
+# B %*% Binv 
+#
+# must be I identity matrix (ones on diagonal, rest zeros)
+#
+#[,1] [,2]
+# [1,]    1    0
+# [2,]    0    1
+#
+
+## Create a cached matrix, with inverse matrix placeholder
 makeCacheMatrix <- function(x = matrix()) {
-
+	inverseMatrix <- NULL
+	set <- function(z) {
+	   x <<- z
+	   m <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(inv) inverseMatrix <<- inv
+	getinverse <- function() inverseMatrix
+	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Calculating the inverse Matrix of the special cached Matrix object
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
